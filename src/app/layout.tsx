06fb26062941f43a4d5f74fcce7841d55d7568bb
Alec <alecfitzgerald90v2.0@gmail.com>
2025-10-24 14:49:39 -0400

    new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased pointer-events-auto`}>
-        <Header />
+        {/* <Header /> */}
         <main className="pt-16">{children}</main>
       </body>
     </html>
